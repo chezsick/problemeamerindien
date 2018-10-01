a20991673b4fdb22261af34c187e993dc1b806c2
Sick <chezsick@bbox.fr>
2018-10-01 21:25:02 +0200

    init

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+$title = "Bienvenue Dans Les Problème Amerindiennes";
+$content = "Bonjour J'addresse Aux Musique Amerindiennes Et Plus Particulièrement Les Pointage Comme J'apelle C'est Des Concert Dans Les Rue, Et Pas Seulement D'ou Ca Vient Mais Aussi Dans Le Monde Entier, Ils Partagent Même Pas Que Ce Soit Les Instrument, Les Tenu Ou Ils Ont Acheté Pour Eux, Ils Vous Réponde Pas Une Question Par E-Mail Je Crois Seulement Les Encouragement Et Récompense, Mais C'est Tout Et J'en Suis Pas Sûr";
+include("include.php");
